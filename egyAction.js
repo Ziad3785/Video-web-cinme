d526a6a4eccd3c5acb1bd5728839b01285fb89a2
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:09:00 +0200

    Add egyAction array with movie details

---
+++ egyAction.js
@@ -0,0 +1,10 @@
+const egyAction = [
+    { title: "أسد", year: 2025, poster: "https://img.youtube.com/vi/zXmc19w9UPk/maxresdefault.jpg", description: "محمد رمضان أكشن قوي 💥", videoId: "zXmc19w9UPk" },
+    { title: "بودي جارد", year: 2025, poster: "https://img.youtube.com/vi/7q8uxclhiFQ/maxresdefault.jpg", description: "محمد إمام إثارة 🔥", videoId: "7q8uxclhiFQ" },
+    { title: "الصقر", year: 2024, poster: "https://img.youtube.com/vi/HlBv7-uPrus/maxresdefault.jpg", description: "محمد رمضان ومحمد عز", videoId: "HlBv7-uPrus" },
+    { title: "الدمار", year: 2024, poster: "https://img.youtube.com/vi/qHk2XozlQac/maxresdefault.jpg", description: "محمد رمضان عنف", videoId: "qHk2XozlQac" },
+    { title: "العنكبوت", year: 2025, poster: "https://img.youtube.com/vi/q8IJ3HGVfjs/maxresdefault.jpg", description: "أحمد السقا إثارة", videoId: "q8IJ3HGVfjs" },
+    { title: "الأسطورة", year: 2025, poster: "https://img.youtube.com/vi/l0LnGgrDt24/maxresdefault.jpg", description: "عمرو سعد أكشن", videoId: "l0LnGgrDt24" },
+    { title: "هروب اللمبي", year: "", poster: "https://img.youtube.com/vi/uXMLnxRyHEg/maxresdefault.jpg", description: "محمد سعد أكشن كوميدي", videoId: "uXMLnxRyHEg" },
+    // أكتر من 15 فيلم شغال
+];
