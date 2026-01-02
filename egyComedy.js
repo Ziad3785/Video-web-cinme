fcebd198f03912815fafee397a6a4e594c6b763f
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:07:31 +0200

    Add egyComedy.js with comedy movie details

---
+++ egyComedy.js
@@ -0,0 +1,7 @@
+const egyComedy = [
+    { title: "ظباط بالوراثة", year: 2024, poster: "https://img.youtube.com/vi/OVV7MgkCwhI/maxresdefault.jpg", description: "محمد إمام كوميديا 😂", videoId: "OVV7MgkCwhI" },
+    { title: "خارج عن السيطرة", year: 2025, poster: "https://img.youtube.com/vi/xs5h3UJdHNE/maxresdefault.jpg", description: "محمد ثروت ومحمد سلام", videoId: "xs5h3UJdHNE" },
+    { title: "تختوخ هنيدي", year: 2024, poster: "https://img.youtube.com/vi/vcS49Wa9xZ4/maxresdefault.jpg", description: "محمد هنيدي نار", videoId: "vcS49Wa9xZ4" },
+    { title: "رمضان مبروك أبو العلمين حمودة", year: "", poster: "https://img.youtube.com/vi/sSN_bIri5lg/maxresdefault.jpg", description: "محمد هنيدي كلاسيك 😂", videoId: "sSN_bIri5lg" },
+    // أكتر من 20 فيلم
+];
