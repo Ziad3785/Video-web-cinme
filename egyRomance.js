8795ffa373795545490e0e59e578df28201911d0
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:08:14 +0200

    Add egyRomance.js with romantic movie data

---
+++ egyRomance.js
@@ -0,0 +1,5 @@
+const egyRomance = [
+    { title: "خلوة خاصة", year: 2024, poster: "https://img.youtube.com/vi/5IfZ5vIywGU/maxresdefault.jpg", description: "هاني سلامة رومانسي ❤️", videoId: "5IfZ5vIywGU" },
+    { title: "A Cowboy Christmas (مصري)", year: 2025, poster: "https://img.youtube.com/vi/9IcNokU6Y7Q/maxresdefault.jpg", description: "رومانسي ريفي حلو", videoId: "9IcNokU6Y7Q" },
+    // أكتر من 15
+];
